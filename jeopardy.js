769ac3d9408fd5a81f5aea41b208d38c13b2e719
Danny Avery <danny.w.avery@gmail.com>
2020-07-02 20:35:57 -0500

    solution

--- jeopardy.js
+++ jeopardy.js
@@ -1,98 +1,135 @@
-// categories is the main data structure for the app; it looks like this:
-
-//  [
-//    { title: "Math",
-//      clues: [
-//        {question: "2+2", answer: 4, showing: null},
-//        {question: "1+1", answer: 2, showing: null}
-//        ...
-//      ],
-//    },
-//    { title: "Literature",
-//      clues: [
-//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
-//        {question: "Bell Jar Author", answer: "Plath", showing: null},
-//        ...
-//      ],
-//    },
-//    ...
-//  ]
-
+const startBtn = document.querySelector("#startGame");
+const WIDTH = 6;
+const HEIGHT = 5;
 let categories = [];
 
-
 /** Get NUM_CATEGORIES random category from API.
- *
  * Returns array of category ids
  */
-
-function getCategoryIds() {
-}
+const getCategoryIds = () => {
+  let randomNum = Math.round(Math.random() * 100);
+  const res = axios.get(
+    `https://jservice.io/api/categories?count=${WIDTH}&offset=${randomNum}`
+  );
+  return Promise.resolve(res).then((categories) => {
+    return categories.data.map((category) => category.id);
+  });
+};
 
 /** Return object with data about a category:
- *
  *  Returns { title: "Math", clues: clue-array }
- *
- * Where clue-array is:
- *   [
- *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
- *      {question: "Bell Jar Author", answer: "Plath", showing: null},
- *      ...
- *   ]
  */
+const getCategory = (catId) => {
+  const result = axios.get(`https://jservice.io/api/clues?category=${catId}`);
+  return Promise.resolve(result).then(({ data }) => {
+    let clues;
+    if (data.length > 5) {
+      // (data.length - HEIGHT) to make sure # does not exceed data.length
+      let randomNum = Math.round(Math.random() * (data.length - HEIGHT));
+      // start at randomNum, add HEIGHT to randomNum for desired # data
+      clues = data.slice(randomNum, randomNum + HEIGHT); // random data
+    } else {
+      clues = data;
+    }
+    hideLoadingView(); // only place that works
+    return { title: clues[0].category.title, clues };
+  });
+};
 
-function getCategory(catId) {
-}
+/** Fill the HTML table#jeopardy with the categories & cells for questions. */
+const fillTable = (categories) => {
+  // showLoadingView();
+  const thead = document.querySelector("thead tr");
+  const tbody = document.querySelector("tbody");
+  let count = 0;
 
-/** Fill the HTML table#jeopardy with the categories & cells for questions.
- *
- * - The <thead> should be filled w/a <tr>, and a <td> for each category
- * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
- *   each with a question for each category in a <td>
- *   (initally, just show a "?" where the question/answer would go.)
- */
+  for (const [index, category] of categories.entries()) {
+    const th = document.createElement("th");
+    const tr = document.createElement("tr");
 
-async function fillTable() {
-}
+    th.innerText = category.title;
+    thead.append(th);
 
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
+    let i = 0;
+    while (i < WIDTH) {
+      const question = categories[i].clues[count].question;
+      const answer = categories[i].clues[count].answer;
 
-function handleClick(evt) {
-}
+      const td = document.createElement("td");
+      td.innerText = "?";
+      td.setAttribute("data-question", question);
+      td.setAttribute("data-answer", answer);
+      tr.appendChild(td);
+      i++;
+    }
+
+    // 1 less row as cats are = to HEIGHT(5)
+    if (index < HEIGHT) {
+      tbody.append(tr);
+      count++;
+    }
+  }
+
+  // hideLoadingView(); // not working
+};
+
+/** Handle clicking on a clue: show the question or answer. */
+const handleClick = (evt) => {
+  const el = evt.target;
+  if (el.classList.contains("question")) {
+    el.classList.remove("question");
+    el.innerText = el.dataset.answer;
+    el.classList.add("answer");
+  } else if (el.classList.contains("answer")) {
+    return;
+  } else {
+    el.innerText = el.dataset.question;
+    el.classList.add("question");
+  }
+};
 
 /** Wipe the current Jeopardy board, show the loading spinner,
  * and update the button used to fetch data.
  */
-
-function showLoadingView() {
-
-}
+const showLoadingView = () => {
+  document.querySelector("button").innerText = "Loading...";
+  document.querySelector("i").classList.remove("hide");
+  document.querySelector("thead tr").innerHTML = "";
+  document.querySelector("tbody").innerHTML = "";
+};
 
 /** Remove the loading spinner and update the button used to fetch data. */
+const hideLoadingView = () => {
+  document.querySelector("button").innerText = "Restart";
+  document.querySelector("i").classList.add("hide");
+};
 
-function hideLoadingView() {
-}
+/** Start game */
+const setupAndStart = async () => {
+  const categoryIds = await getCategoryIds();
 
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
+  const categories = [];
+  for (const categoryId of categoryIds) {
+    const category = await getCategory(categoryId);
+    categories.push(category);
+  }
 
-async function setupAndStart() {
-}
+  await fillTable(categories);
+
+  // await hideLoadingView(); // not working
+};
 
 /** On click of start / restart button, set up game. */
-
-// TODO
+startBtn.addEventListener("click", () => {
+  showLoadingView();
+  setupAndStart();
+  // hideLoadingView(); // not working
+});
 
 /** On page load, add event handler for clicking clues */
-
-// TODO
+document.addEventListener("DOMContentLoaded", () => {
+  const board = document.querySelector("#board");
+  board.addEventListener("click", (evt) => {
+    if (evt.target.tagName == "TD") handleClick(evt);
+  });
+});
